54c1ab3ba19ad8c10c8c82b9b4a7e70b66939aac
Salil Upadhyay <upsalil.01@gmail.com>
2023-05-25 13:08:15 +0530

    deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const jsonserver = require("json-server");
-const server = jsonserver.create();
-const router = jsonserver.router();
-const middlwares = jsonserver.defaults();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlwares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlwares);
